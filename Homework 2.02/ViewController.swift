3ec5dac28899f28d822334bae602d00e9c48e6fa
Nick <wizard228322@icloud.com>
2023-03-18 12:24:41 +0400

    Initial Commit

---
+++ Homework 2.02/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework 2.02
+//
+//  Created by 65 on 18.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
