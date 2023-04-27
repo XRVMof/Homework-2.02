c9225708334fea2961bb598b013a2a617c256cec
Nick <wizard228322@icloud.com>
2023-04-27 16:00:05 +0400

    test commit

--- Homework 2.02/ViewController.swift
+++ Homework 2.02/ViewController.swift
@@ -7,36 +7,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+    
     @IBOutlet weak var redSignalView: UIView!
     @IBOutlet weak var yellowSignalView: UIView!
     @IBOutlet weak var greenSignalView: UIView!
+    
     @IBOutlet weak var startButton: UIButton!
     
-    
+    private let signalLightIsOn: CGFloat = 1
+    private let signalLightIsOff: CGFloat = 0.3
+
     override func viewDidLoad() {
         super.viewDidLoad()
         redSignalView.layer.cornerRadius = redSignalView.frame.width / 2
         yellowSignalView.layer.cornerRadius = yellowSignalView.frame.width / 2
         greenSignalView.layer.cornerRadius = greenSignalView.frame.width / 2
+        
         startButton.layer.cornerRadius = 10
     }
     
     @IBAction func startButtonDidTapped() {
         startButton.setTitle("NEXT", for: .normal)
-        greenSignalView.alpha = 1
-        if yellowSignalView.alpha == 1 {
-            yellowSignalView.alpha = 0.3
-            redSignalView.alpha = 0.3
-            greenSignalView.alpha = 1
-        } else if redSignalView.alpha == 1 {
-            redSignalView.alpha = 0.3
-            greenSignalView.alpha = 0.3
-            yellowSignalView.alpha = 1
-        } else if greenSignalView.alpha == 1 {
-            greenSignalView.alpha = 0.3
-            yellowSignalView.alpha = 0.3
-            redSignalView.alpha = 1
+        greenSignalView.alpha = signalLightIsOn
+        if yellowSignalView.alpha == signalLightIsOn {
+            yellowSignalView.alpha = signalLightIsOff
+            redSignalView.alpha = signalLightIsOff
+            greenSignalView.alpha = signalLightIsOn
+        } else if redSignalView.alpha == signalLightIsOn {
+            redSignalView.alpha = signalLightIsOff
+            greenSignalView.alpha = signalLightIsOff
+            yellowSignalView.alpha = signalLightIsOn
+        } else if greenSignalView.alpha == signalLightIsOn {
+            greenSignalView.alpha = signalLightIsOff
+            yellowSignalView.alpha = signalLightIsOff
+            redSignalView.alpha = signalLightIsOn
         }
     }
 }
